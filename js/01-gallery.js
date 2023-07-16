a4f063d0760e7f60234245794779b573d58e8c2c
Olga067965 <olgasavelieva5@gmail.com>
2023-07-16 21:53:06 +0300

    Update3

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+function createGalleryItemMarkup({ preview, original, description }) {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+      </a>
+    </li>
+  `;
+}
+
+const galleryMarkup = galleryItems
+  .map((item) => createGalleryItemMarkup(item))
+  .join("");
+
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryList.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const { target } = event;
+
+  if (target.classList.contains("gallery__image")) {
+    const source = target.dataset.source;
+
+    const instance = basicLightbox.create(`
+      <img src="${source}" alt="${target.alt}" />
+    `);
+
+    instance.show();
+  }
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    basicLightbox.close();
+  }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+function createGalleryItemMarkup({ preview, original, description }) {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>
+  `;
+}
+
+const galleryMarkup = galleryItems
+  .map((item) => createGalleryItemMarkup(item))
+  .join("");
+
+galleryList.innerHTML = galleryMarkup;
+
+const gallery = new SimpleLightbox(".gallery__link", {
+  captions: true,
+  captionDelay: 250,
+});
